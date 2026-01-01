1460f1d2bc0afcadd62243c7df21f3ff04598891
akashornet <akash.salekar@ornet.co.in>
2026-01-01 21:21:04 +0530

    fibo

---
+++ Fibo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Fibo {
+
+    static int fibonacci(int total){
+        int n1=0,n2=1 ,n3;
+          System.out.print(n1 + "  " + n2);
+        for(int i=2 ;  i< total ;i++){
+            n3 = n1 +n2;
+         System.out.print(" "+ n3);
+           n1 = n2;
+           n2 = n3;
+           
+        }
+       
+        return total;
+    }
+
+    public static void main(String[] args) {
+     Fibo.fibonacci(10);
+    
+
+    }
+}
