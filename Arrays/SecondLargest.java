88c07ea1d755ae7a4677a4a6fc0e0795fd02e006
arjun98k <arjunkandekar14@gmail.com>
2026-01-23 19:15:57 +0530

    Second Largest number

---
+++ Arrays/SecondLargest.java
@@ -0,0 +1,27 @@
+public class SecondLargest {
+    public int secondlargest(int []arr){
+        if (arr.length <2) {
+            throw new IllegalArgumentException("Array must have at least 2 elements");
+        }
+        int largest= Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int currentnum = 0; currentnum < arr.length; currentnum++) {
+            if (arr[currentnum] > largest ) {
+               secondLargest = largest;
+                largest = arr[currentnum];
+            }
+            else if (arr[currentnum] > secondLargest && arr[currentnum]!=largest) {
+                secondLargest= arr[currentnum];
+            }
+        }
+        return secondLargest;
+    }
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 20, 8};
+
+        SecondLargest obj = new SecondLargest();
+        int result = obj.secondlargest(arr);
+        System.out.println("Second Largest = " + result);
+    }
+}
