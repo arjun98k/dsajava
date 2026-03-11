c8a7d1ab6f6cb226c52f743eff1259ffdcb3999d
Arjun98 <arjunkandekar14@gmail.com>
2026-03-11 16:16:38 +0530

    vizpay prime number

---
+++ OneDrive/Desktop/devprog/vizpay/NumberSystem/PrimeNumberr.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+
+
+public class PrimeNumberr {
+
+    static boolean  primeNumber(int num){
+        if (num == 0 && num ==1) {
+            return false;
+        }
+        for (int i = 2; i*i < num; i++){
+            if (num%i == 0) {
+               return  false;
+            } 
+            
+        } return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int input = obj.nextInt();
+        if (primeNumber(input) ) {
+             System.out.println("it is prime number");
+        } else {
+            System.out.println("it not prime number");
+        }
+    }
+        }
+        
+        
+    
+
